d717ab5cd585172c0b1d34dc51617453e153fe76
fabiofa87 <fabiofadev87@gmail.com>
2022-02-05 11:37:43 -0300

    Created DB and updated

---
+++ cmd/web/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"letsgo.com/snippetbox/pkg/models/mysql"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	dsn := flag.String("dsn", "fabio-faria:gAw5%lm7%L@teKFi@m@N@(35.226.146.116:3306)/maryam-fabio-faria", "MySQL data source name")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Println("Starting server on", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
